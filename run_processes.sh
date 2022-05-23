3606b5e2cd7880a210f780f6091463fc17d2bd7c
MathPelicer <matheuspelicer58@gmail.com>
2022-05-23 11:06:46 -0300

    [code] bash script for all clients

---
+++ run_processes.sh
@@ -0,0 +1,10 @@
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/oil_res_client.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/EtOH_tank_client.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/NaOH_tank_client.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/reactor_client.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/decanter_client.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/dryer_client.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/washer_client_0.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/washer_client_1.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/washer_client_2.py & 
+python C:/Users/mathe/Documents/FEI/7_semester/dist_system/CC7261_Project/elements/bio_dryer_client.py & 
